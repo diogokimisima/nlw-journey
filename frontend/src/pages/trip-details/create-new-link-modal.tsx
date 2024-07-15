e8bf80eda03734473ddebccee87cb4de7c817eea
dkimisima@gmail.com <dkimisima@gmail.com>
2024-07-15 10:44:15 -0300

    creating new Link

--- frontend/src/pages/trip-details/create-new-link-modal.tsx
+++ frontend/src/pages/trip-details/create-new-link-modal.tsx
@@ -1,5 +1,8 @@
-import { Calendar, Tag, X } from "lucide-react";
+import {  Link, Plus, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
+import { FormEvent } from "react";
 
 interface CreateNewLinkModalProps {
     closeCreateNewLinkModalOpen: () => void
@@ -8,13 +11,32 @@ interface CreateNewLinkModalProps {
 export function CreateNewLinkModal({
     closeCreateNewLinkModalOpen
 }: CreateNewLinkModalProps) {
-   
+
+    const { tripId } = useParams()
+
+    async function createLink(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()  
+
+        const data = new FormData(event.currentTarget)
+
+        const title = data.get('title')?.toString()
+        const url = data.get('url')?.toString()
+
+        await api.post(`/trips/${tripId}/links`, {
+            title,
+            url,
+        })
+
+        window.document.location.reload()
+    }
+
+
     return (
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
             <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
                 <div className='space-y-2'>
                     <div className='flex items-center justify-between'>
-                        <h2 className='text-lg font-semibold'>Cadastrar atividade</h2>
+                        <h2 className='text-lg font-semibold'>Cadastrar novo Link</h2>
                         <button onClick={closeCreateNewLinkModalOpen}>
                             <X className='size-5 text-zinc-400' />
                         </button>
@@ -25,31 +47,28 @@ export function CreateNewLinkModal({
                 </div>
                 <div className='w-full h-px bg-zinc-800'></div>
 
-                <form onSubmit={closeCreateNewLinkModalOpen} className='space-y-3'>
+                <form onSubmit={createLink} className='space-y-3'>
                     <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
                         <Tag className='text-zinc-400 size-5' />
                         <input
                             name='title'
-                            placeholder="Qual sua atividade"
+                            placeholder="Titulo"
+                            className="bg-transparent text-lg placeholder:-zinc-400 outline-none flex-1"
+                        />
+                    </div>
+                    <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                        <Link className='text-zinc-400 size-5' />
+                        <input
+                            name='url'
+                            placeholder="Link"
                             className="bg-transparent text-lg placeholder:-zinc-400 outline-none flex-1"
                         />
                     </div>
 
-                    <div className="flex items-center gap-2">
-                        <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center flex-1 gap-2'>
-                            <Calendar />
-                            <input
-                                type="datetime-local"
-                                name='occurs_at'
-                                placeholder="Data e horário da atividade"
-                                className="bg-transparent text-lg placeholder:-zinc-400 outline-none flex-1 "
-                            />
-                        </div>
-
-                    </div>
 
                     <Button size="full">
-                        Salvar atividade
+                        <Plus />
+                        Novo Link
                     </Button>
                 </form>
             </div>
--- frontend/src/pages/trip-details/important-links.tsx
+++ frontend/src/pages/trip-details/important-links.tsx
@@ -1,10 +1,21 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CreateNewLinkModal } from "./create-new-link-modal";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface Links {
+    id: string
+    title: string
+    url: string
+}
 
 export function ImportantLinks() {
     const [isCreateNewLinkModalOpen, setIsCreateNewLinkModalOpen] = useState(false)
+    const [links, setLinks] = useState<Links[]>([])
+
+    const { tripId } = useParams()
 
     function openCreateNewLinkModalOpen() {
         setIsCreateNewLinkModalOpen(true)
@@ -14,29 +25,28 @@ export function ImportantLinks() {
         setIsCreateNewLinkModalOpen(false)
     }
 
+    useEffect(() => {
+        api.get(`/trips/${tripId}/links`).then(response => setLinks(response.data.links))
+
+    }, [tripId])
 
     return (
         <div className="space-y-6">
             <h2 className="font-semibold text-xl">Links importantes</h2>
             <div className="space-y-5">
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5 flex-1">
-                        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-                        <a href="#" className="block text-sm text-zinc-400 truncate hover:text-zinc-500"
-                        >https://www.airbnb.com.br/rooms/104700011312414512514512551241231231
-                        </a>
-                    </div>
-                    <Link2 className="size-5 text-zinc-400" />
-                </div>
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5 flex-1">
-                        <span className="block font-medium text-zinc-100">Regras da casa</span>
-                        <a href="#" className="block text-sm text-zinc-400 truncate hover:text-zinc-500"
-                        >https://www.notion.com/pages/1047000112354648336?adults=13&children=0&infants=0&pets=0&wishlist_item_id=11003621872995&check_in=2024-08-17&check_out=2024-08-26&source_impression_id=p3_1717600906_P3DL0E-bJZzguEci&previous_page_section_name=1000
-                        </a>
-                    </div>
-                    <Link2 className="size-5 text-zinc-400" />
-                </div>
+                {links.map(link => {
+                    return (
+                        <div key={link.id} className="flex items-center justify-between gap-4">
+                            <div className="space-y-1.5 flex-1">
+                                <span className="block font-medium text-zinc-100">{link.title}</span>
+                                <a href="#" className="block text-sm text-zinc-400 truncate hover:text-zinc-500"
+                                >{link.url}
+                                </a>
+                            </div>
+                            <Link2 className="size-5 text-zinc-400" />
+                        </div>
+                    )
+                })}
             </div>
 
             <Button onClick={openCreateNewLinkModalOpen} variant="secondary" size="full">
